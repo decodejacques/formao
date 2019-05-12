26314d7f18aa5419b8d67b919ecf137872468c8e
Jacques Le Normand <jacqueslenormand@gmail.com>
2019-05-12 13:47:42 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+let pictures = ["/img1.jpg",
+    "/img2.jpg"
+]
+
+import React, { Component } from 'react'
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            pictureIndex: 0
+        }
+    }
+    componentDidMount = () => {
+        this.intervalId = setInterval(this.nextImage, 1000)
+    }
+    componentWillUnmount = () => {
+        cancelInterval(this.intervalId)
+    }
+    nextImage = () => {
+        this.setState({ pictureIndex: this.state.pictureIndex + 1 })
+    }
+    render = () => {
+        return (<div className="container">
+            <img
+                className="scroll-img"
+                key={this.state.pictureIndex}
+                src={pictures[this.state.pictureIndex % pictures.length]} />
+            <img
+                className="scroll-img scroll-animated"
+                key={this.state.pictureIndex + 1}
+                src={pictures[(this.state.pictureIndex + 1) % pictures.length]} />
+        </div>)
+    }
+}
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom'
+import './main.css'
+import React, { Component } from 'react'
+import App from './App.jsx'
+ReactDOM.render(<App />, document.getElementById("root"))
